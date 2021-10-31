44521142f8e4666904ba87e41f7d75f1ee89d24a
Yiivon <i@yiivon.me>
2021-11-01 01:41:49 +0800

    DEV: Moving on

--- src/nodes/instances.js
+++ src/nodes/instances.js
@@ -1,4 +1,3 @@
-
 function MT4Client() {
     this.size = [60, 20];
     this.addInput("stop", LiteGraph.ACTION, {label: '停止'});
@@ -18,71 +17,30 @@ function MT4Client() {
 }
 
 MT4Client.title = "MT4实例";
-MT4Client.desc = "Send data through a websocket";
+MT4Client.desc = "MT4终端实例，可接收报价交易事件并能进行实时下单交易。";
 
-MT4Client.prototype.onPropertyChanged = function(name, value) {
+MT4Client.prototype.onPropertyChanged = function (name, value) {
     if (name === "url") {
         this.connectSocket();
     }
 };
 
-MT4Client.prototype.onExecute = function() {
-    if (!this._ws && this.properties.url) {
-        this.connectSocket();
-    }
+MT4Client.prototype.onExecute = function () {
 
-    if (!this._ws || this._ws.readyState !== WebSocket.OPEN) {
-        return;
-    }
-
-    let room = this.properties.room;
-    let only_changes = this.properties.only_send_changes;
-
-    for (let i = 1; i < this.inputs.length; ++i) {
-        let data = this.getInputData(i);
-        if (data == null) {
-            continue;
-        }
-        let json;
-        try {
-            json = JSON.stringify({
-                type: 0,
-                room: room,
-                channel: i,
-                data: data
-            });
-        } catch (err) {
-            continue;
-        }
-        if (only_changes && this._last_sent_data[i] == json) {
-            continue;
-        }
-
-        this._last_sent_data[i] = json;
-        this._ws.send(json);
-    }
-
-    for (let i = 1; i < this.outputs.length; ++i) {
-        this.setOutputData(i, this._last_received_data[i]);
-    }
-
-    if (this.boxcolor === "#AFA") {
-        this.boxcolor = "#6C6";
-    }
 };
 
-MT4Client.prototype.connectSocket = function() {
+MT4Client.prototype.connectSocket = function () {
     let that = this;
     let url = this.properties.url;
     if (url.substr(0, 2) !== "ws") {
         url = "ws://" + url;
     }
     this._ws = new WebSocket(url);
-    this._ws.onopen = function() {
+    this._ws.onopen = function () {
         console.log("ready");
         that.boxcolor = "#6C6";
     };
-    this._ws.onmessage = function(e) {
+    this._ws.onmessage = function (e) {
         that.boxcolor = "#AFA";
         let data = JSON.parse(e.data);
         if (data.room && data.room !== that.properties.room) {
@@ -107,25 +65,25 @@ MT4Client.prototype.connectSocket = function() {
             that._last_received_data[data.channel || 0] = data.data;
         }
     };
-    this._ws.onerror = function(e) {
+    this._ws.onerror = function (e) {
         console.log("couldnt connect to websocket");
         that.boxcolor = "#E88";
     };
-    this._ws.onclose = function(e) {
+    this._ws.onclose = function (e) {
         console.log("connection closed");
         that.boxcolor = "#000";
     };
 };
 
-MT4Client.prototype.send = function(data) {
-    if (!this._ws || this._ws.readyState != WebSocket.OPEN) {
+MT4Client.prototype.send = function (data) {
+    if (!this._ws || this._ws.readyState !== WebSocket.OPEN) {
         return;
     }
-    this._ws.send(JSON.stringify({ type: 1, msg: data }));
+    this._ws.send(JSON.stringify({type: 1, msg: data}));
 };
 
-MT4Client.prototype.onAction = function(action, param) {
-    if (!this._ws || this._ws.readyState != WebSocket.OPEN) {
+MT4Client.prototype.onAction = function (action, param) {
+    if (!this._ws || this._ws.readyState !== WebSocket.OPEN) {
         return;
     }
     this._ws.send({
@@ -136,12 +94,12 @@ MT4Client.prototype.onAction = function(action, param) {
     });
 };
 
-MT4Client.prototype.onGetInputs = function() {
-    return [["in", 0]];
+MT4Client.prototype.onGetInputs = function () {
+    return [];
 };
 
-MT4Client.prototype.onGetOutputs = function() {
-    return [["out", 0]];
+MT4Client.prototype.onGetOutputs = function () {
+    return [];
 };
 
 LiteGraph.registerNodeType("实例/MT4", MT4Client);
