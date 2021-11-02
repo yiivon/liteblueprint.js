c32ef8008513b4989a3a5bd93983484e4cadad8f
Yiivon <i@yiivon.me>
2021-11-02 22:53:18 +0800

    DEV: Moving on

--- src/litegraph.js
+++ src/litegraph.js
@@ -7846,7 +7846,6 @@ LGraphNode.prototype.executeAction = function(action)
                             if(slot.type !== LiteGraph.EVENT) {
                                 ctx.font =  "normal " + LiteGraph.NODE_SUBTEXT_SIZE-2 + "px Arial";
                             }
-
                             if (horizontal || slot.dir === LiteGraph.UP) {
                                 ctx.fillText(text, pos[0], pos[1] - 10);
                             } else {
@@ -7935,7 +7934,10 @@ LGraphNode.prototype.executeAction = function(action)
                         let text = slot.label != null ? slot.label : slot.name;
                         if (text) {
                             ctx.fillStyle = LiteGraph.NODE_TEXT_COLOR;
-                            if (horizontal || slot.dir == LiteGraph.DOWN) {
+                            if(slot.type !== LiteGraph.EVENT) {
+                                ctx.font =  "normal " + LiteGraph.NODE_SUBTEXT_SIZE-2 + "px Arial";
+                            }
+                            if (horizontal || slot.dir === LiteGraph.DOWN) {
                                 ctx.fillText(text, pos[0], pos[1] - 8);
                             } else {
                                 ctx.fillText(text, pos[0] - 10, pos[1] + 5);
