a606379146c16a010d08e3581159c0e04cdc6b1b
Yiivon <i@yiivon.me>
2021-11-03 03:20:39 +0800

    DEV: Moving on

--- src/nodes/connectors.js
+++ src/nodes/connectors.js
@@ -52,7 +52,7 @@ Concentrator.prototype.onConnectionsChange = function (type,
 
                 let v = schema[p];
                 v = (typeof v === 'string' ? {type: v} : v);
-                this.addOutput(p, v.type, v);
+                this.addInput(p, v.type, v);
             }
         }
     }
